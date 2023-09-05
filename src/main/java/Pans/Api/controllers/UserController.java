e760bec39749693e6f6c4a6727042ac2449d157d
RafixOOO <smoktbg@gmail.com>
2023-09-05 13:45:31 +0200

    api

--- src/main/java/Pans/Api/controllers/UserController.java
+++ src/main/java/Pans/Api/controllers/UserController.java
@@ -1,13 +1,19 @@
 package Pans.Api.controllers;
 
+import Pans.Api.exceptions.ResourceNotFoundException;
+import Pans.Api.models.Event;
 import Pans.Api.models.KoloUser;
+import Pans.Api.models.Person;
 import Pans.Api.models.User;
 import Pans.Api.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/User")
@@ -24,5 +30,26 @@ public class UserController {
     @PostMapping("/Add")
     public User createKoloUser(@Valid @RequestBody User user) {
         return userRepository.save(user);
+    }
+
+    @PutMapping("/Edit")
+    public User updatePerson(@RequestBody User user){
+        User existingUser=userRepository.findById(user.getId()).orElse(null);
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String hashedPassword = encoder.encode(user.getPassword());
+        existingUser.setPassword(hashedPassword);
+        return userRepository.save(existingUser);
+    }
+
+    @DeleteMapping("/Delete/{id}")
+    public Map<String, Boolean> deleteEvent(@PathVariable(value = "id") Integer userId)
+            throws ResourceNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Event not found for this id :: " + userId));
+
+        userRepository.delete(user);
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("deleted", Boolean.TRUE);
+        return response;
     }
 }
