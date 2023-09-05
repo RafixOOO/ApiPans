cc50f4b6bf7d54da06c2578aa1c23d7261f61d0e
RafixOOO <smoktbg@gmail.com>
2023-09-05 13:38:55 +0200

    api

--- src/main/java/Pans/Api/controllers/UserController.java
+++ src/main/java/Pans/Api/controllers/UserController.java
@@ -1,8 +1,11 @@
 package Pans.Api.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import Pans.Api.models.KoloUser;
+import Pans.Api.models.User;
+import Pans.Api.repository.UserRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
@@ -10,8 +13,16 @@ import java.security.Principal;
 @RequestMapping("/api/User")
 public class UserController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/Current")
     public String getLoggedInUser(Principal principal){
         return principal.getName();
+    }
+
+    @PostMapping("/Add")
+    public User createKoloUser(@Valid @RequestBody User user) {
+        return userRepository.save(user);
     }
 }
