bbf3009e51a39210a45db69e599403a8bfb461ef
RafixOOO <smoktbg@gmail.com>
2023-09-21 09:46:18 +0200

    api

--- src/main/java/Pans/Api/config/SecurityConfig.java
+++ src/main/java/Pans/Api/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+
         http.csrf(csrf -> csrf.disable())
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth->
